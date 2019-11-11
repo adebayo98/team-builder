0e15201d2718e741af7f0bf8b295a0ed6749e87f
Dimitri <Dimitri.vildina@gmail.com>
2019-11-11 14:37:11 +0100

    page connexion sans styles

---
+++ client/src/components/ProfileCard.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class ProfileCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+
+    render() {
+
+        return(
+            <div className={'profile-card'}>
+                <img src={this.props.img} alt={this.props.name} className={'profile-card__img'}></img>
+                <p className={'profile-card__name'}>{this.props.name}</p>
+                <p className={'profile-card__job'}>{this.props.job}</p>
+            </div>
+        );
+    }
+
+}
+
+export default ProfileCard;
--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -2,10 +2,36 @@ import React from 'react';
 
 /* COMPONENTS */
 
+import ProfileCard from '../components/ProfileCard';
+import InputEmail from '../components/ui/InputEmail';
+import InputPassword from '../components/ui/InputPassword';
+import ButtonComponent from '../components/ui/ButtonComponent';
+
 const HomeView = () => {
     return(
-        <div className={'home-view'}>
-            
+        <div className={'home-view d-f'}>
+            <div className={'home-left d-f'}>
+                <ProfileCard 
+                    img =""
+                    name="Dimitri"
+                    job="Dév"
+                />
+                <ProfileCard />
+                <ProfileCard />
+            </div>
+            <div className={'home-right'}>
+                <img className={'home-right__logo'} src={'/images/logo-3-blanc.svg'} alt={'Logo Hetic'} />
+                <h2>Connexion</h2>
+                <InputEmail />
+                <InputPassword />
+                <ButtonComponent 
+                    label="Connexion"
+                />
+                <a href="">Impossible de se connecter</a>
+                <ButtonComponent 
+                    label="Inscription"
+                />
+            </div>
         </div>
     );
 }
