8998a95ebc37711d6531d45859a7eb3ce56cf0c3
Dimitri <Dimitri.vildina@gmail.com>
2019-11-11 18:32:49 +0100

    Home view v1

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -21,21 +21,23 @@ const HomeView = () => {
             </div>
             <div className={'home-right container-fluid d-f'}>
                 <img className={'home-right__logo'} src={'/images/logo-3-blanc.svg'} alt={'Logo Hetic'} />
-                <h2>Connexion</h2>
-                <InputEmail 
-                    label="Email*"
-                />
-                <InputPassword 
-                    label="Mot de passe*"
-                />
-                <ButtonComponent
-                    label="Connexion"
-                />
-                <a href="">Impossible de se connecter</a>
-                <ButtonComponent
-                    buttonSecond="Yes"
-                    label="Inscription"
-                />
+                <h2 className={'home-right__title'}>Connexion</h2>
+                <form className={'home-right__form d-f'}>
+                    <InputEmail 
+                        label="Email*"
+                    />
+                    <InputPassword 
+                        label="Mot de passe*"
+                    />
+                    <ButtonComponent
+                        label="Connexion"
+                    />
+                    <a href="" className={'home-right__problem'}>Impossible de se connecter</a>
+                    <ButtonComponent
+                        buttonSecond="Yes"
+                        label="Inscription"
+                    />
+                </form>
             </div>
         </div>
     );
