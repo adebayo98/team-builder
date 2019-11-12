da7a1e0b5f4a1b7253c6f3f3309dcd3dd96d8b0c
Dimitri <Dimitri.vildina@gmail.com>
2019-11-12 10:03:58 +0100

    profil header

--- client/src/components/ProfileHeader.js
+++ client/src/components/ProfileHeader.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 
+import ProfilePic from '../assets/img/profile-pic.jpg';
+import ArrowDown from '../assets/img/icons/Arrow down.svg';
+
 class ProfileHeader extends Component {
 
     constructor(props) {
@@ -12,8 +15,13 @@ class ProfileHeader extends Component {
     render() {
 
         return(
-            <div className={'profile-header'}>
-                
+            <div className={'profile-header d-f'}>
+                <div className={'profile-header__user d-f'}>
+                    <p className={'user-fullname'}>Nathan Colin</p>
+                    <p className={'user-role'}>Admin</p>
+                </div>
+                <div className={'profile-header__img'} style={{backgroundImage: `url(${ProfilePic}`}}></div>
+                <img src={ArrowDown} alt="" className={'profile-header__arrow'}></img>
             </div>
         );
     }
