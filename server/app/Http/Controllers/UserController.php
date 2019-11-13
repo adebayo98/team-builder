a77dc120edd8d728f88be4963744fdf8fb06a7ed
404NotFoundError <hountondjigodwill@gmail.com>
2019-11-13 01:28:24 +0100

    Filter init

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Cache;
 
+
 /**
  * Class UserController
  * @package App\Http\Controllers
@@ -22,29 +23,66 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function users()
+    public function users(Request $request)
     {
+        $promotions = $request->query->get('promotions', false);
+        $skills = $request->query->get('skills', false);
+
+        $users = DB::table('users')
+            ->join('formations', 'formations.id', '=', 'users.formation_id')
+            ->join('promotions', 'promotions.id', '=', 'users.promotion_id')
+            ->select('users.id as id','users.photo_url', 'users.last_name', 'users.first_name', 'formations.code as formation', 'promotions.name as promotion')
+            ->when($promotions, function ($query, $promotions) {
+                return $query->whereIn('formations.code', $promotions);
+            })
+            ->when($skills, function ($query, $skills) {
+                foreach ($skills as $key => $skill){
+                    $this->skill = $skill;
+                    $query->whereExists(function ($query) {
+                        $query->select(DB::raw(1))
+                            ->from('user_skill')
+                            ->join('skills', function ($join) {
+                                $join->on('user_skill.skill_id', '=', 'skills.id')
+                                    ->where('skills.name', '=', $this->skill);
+                            });
+                    });
+                }
+                return $query;
+            })
+            ->orderBy('users.first_name', 'asc')
+            ->get();
+
+        dump($users);
+        die;
+
         // Check if cache exist
-        if (!Cache::has('app_user_list')){
-            $users = DB::table('users')
-                ->join('formations', 'formations.id', '=', 'users.formation_id')
-                ->select('users.photo_url', 'users.last_name', 'users.first_name', 'formations.code as formation')
-                ->orderBy('users.first_name', 'asc')
-                ->get();
-            Cache::put('app_user_list', $users, now()->addMinutes(10));
-        }
+//        if (!Cache::has('app_user_list')){
+//            $users = DB::table('users')
+//                ->join('formations', 'formations.id', '=', 'users.formation_id')
+//                ->join('promotions', 'promotions.id', '=', 'users.promotion_id')
+//                ->select('users.photo_url', 'users.last_name', 'users.first_name', 'formations.code as formation', 'promotions.name as promotion')
+//                ->orderBy('users.first_name', 'asc')
+//                ->get();
+//            Cache::put('app_user_list', $users, now()->addMinutes(10));
+//        }
         // Return response
         return response()
             ->json([
                 'status' => 'success',
                 'message' => 'ok',
                 'result' => [
-                    'users' => Cache::get('app_user_list'),
-                    'count' => count(Cache::get('app_user_list')),
+                    'users' => $users, //Cache::get('app_user_list'),
+                    // 'count' => count(Cache::get('app_user_list')),
                 ]
             ], 200);
     }
 
+    /**
+     * Get an user
+     *
+     * @param int $id The user id
+     * @return mixed
+     */
     public function user(int $id)
     {
         $user = User::find($id);
