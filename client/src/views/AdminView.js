53a8c64639dadddf9899caa67cc7249b8159e632
romain <romain78730@hotmail.fr>
2019-11-13 15:28:27 +0100

    fetch data in admin

--- client/src/views/AdminView.js
+++ client/src/views/AdminView.js
@@ -6,9 +6,31 @@ import HeaderBottom from '../components/HeaderBottom';
 
 import ProfileCard from '../components/ProfileCard';
 
-const AdminView = () => {
-    return(
-        <div className={'admin-view'}>
+class AdminView extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            isLoading: true,
+            data: ""
+        };
+    }
+
+    async componentDidMount() {
+        const response = await fetch(`http://hetic.adebayo.fr/api/users`);
+        const json = await response.json();
+        this.setState({ 
+            data : json,
+            isLoading: false
+        });
+    }
+    
+
+    render(){
+        return(
+            <div className={'admin-view'}>
             <section className={'admin-header'}>
                 <HeaderTop />
                 <HeaderBottom 
@@ -16,66 +38,34 @@ const AdminView = () => {
                 />
             </section>
             <section className={'admin-content container-fluid'}>
-                <p className={'admin-content__number'}>3 utilisateurs</p>
-                <div className={'admin-content__cards d-f'}>
-                    <ProfileCard 
-                        class="web3" 
-                        img =""
-                        name="Dimitri"
-                        job="Dév"
-                    />
-                    <ProfileCard 
-                        class="web3" 
-                        img =""
-                        name="Dimitri"
-                        job="Dév"
-                    />
-                    <ProfileCard 
-                        class="web3" 
-                        img =""
-                        name="Dimitri"
-                        job="Dév"
-                    />
-                    <ProfileCard 
-                        class="web3" 
-                        img =""
-                        name="Dimitri"
-                        job="Dév"
-                    />
-                    <ProfileCard 
-                        class="web3" 
-                        img =""
-                        name="Dimitri"
-                        job="Dév"
-                    />
-                    <ProfileCard 
-                        class="web3" 
-                        img =""
-                        name="Dimitri"
-                        job="Dév"
-                    />
-                    <ProfileCard 
-                        class="web3" 
-                        img =""
-                        name="Dimitri"
-                        job="Dév"
-                    />
-                    <ProfileCard 
-                        class="web3" 
-                        img =""
-                        name="Dimitri"
-                        job="Dév"
-                    />
-                    <ProfileCard 
-                        class="web3" 
-                        img =""
-                        name="Dimitri"
-                        job="Dév"
-                    />
+                { this.state.isLoading ? 
+                <div className="ta-c">Loading students</div>
+                : 
+                <div>
+                    <p className={'admin-content__number'}>{this.state.data.result.total} utilisateurs</p>
+                    <div className={'admin-content__cards d-f jc-around'}>
+                    
+                        {this.state.data.result.users.map( item => {
+                            return(
+                                <ProfileCard 
+                                    id= {item.id}
+                                    class= {item.formation} 
+                                    img =""
+                                    name= {item.first_name + ' ' + item.last_name}
+                                    job= {item.role}
+                                />
+                            )
+                        })}
+                        
+                    </div>
                 </div>
+                
+                }
+                
             </section>
         </div>
-    );
+        );
+    }
 }
 
 export default AdminView;
