f720d68a6b340fb4d13932abe1a431d3ede7d71b
404NotFoundError <hountondjigodwill@gmail.com>
2019-11-21 02:45:44 +0100

    Integration \ Styling \ Logic terminate for Module Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import SingleUser from "./pages/SingleUser";
+import Users from "./pages/Users";
+import SessionHelper from "./helpers/SessionHelper";
 
 class App extends React.Component {
 
   constructor(props) {
       super(props);
       this.state = {
+          isAuth: SessionHelper.isAuth(),
       };
   }
 
   render() {
       return(
           <Router>
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/" component={Login} />
-              {/*<Route exact path="/signin" component={() => <SingleFormLayout content={<SignInView/>} />} />*/}
+              {/* List of routes not need auth */}
+              { !this.state.isAuth ? <Route exact path="/" component={Login}/> : '' }
+
+              {/* List of route which need auth */}
+              { this.state.isAuth ?
+                  <div>
+                      <Route exact path="/" component={SessionHelper.hasRole('student') ? SingleUser : Users}/>
+                      <Route exact path="/profile" component={SingleUser}/>
+                      <Route exact path="/login" component={Login}/>
+                  </div>
+              : '' }
           </Router>
       );
   }
--- client/src/helpers/SessionHelper.js
+++ client/src/helpers/SessionHelper.js
@@ -1,16 +1,55 @@
+import AppHelper from "./AppHelpers";
+
 
 const SessionHelper =  {
 
+    /**
+     * The name of access token in local storage.
+     *
+     * @var string
+     */
+    authLocalStorageName: '__xxx-tutnfkin-sbuvyrujz',
+
+    /**
+     * Flush local storage with the current user
+     * access token and reload app to bind auth route.
+     *
+     * @param token (string) The user's token.
+     */
+    build: (token) => {
+        localStorage.setItem(SessionHelper.authLocalStorageName, token);
+        window.location.reload();
+    },
+
+    /**
+     * Check existence of an access token in local storage.
+     *
+     * @returns {boolean}
+     */
     isAuth: () => {
-        return localStorage.getItem('userData') ? true : false;
+        return localStorage.getItem(SessionHelper.authLocalStorageName) ? true : false;
     },
 
+    /**
+     * Get an user data from local storage on access token.
+     *
+     * @returns {*|any[]|boolean|any}
+     */
     userData: () => {
-        return JSON.parse(localStorage.getItem('userData'));
+        return AppHelper.parseJwt(
+            localStorage.getItem(SessionHelper.authLocalStorageName),
+            'payload'
+        );
     },
 
+    /**
+     * Check if the current user as some role.
+     *
+     * @param role (string) The role to verify.
+     * @returns {boolean}
+     */
     hasRole: (role) => {
-        return JSON.parse(localStorage.getItem('userData')).role === "student" ? true : false;
+        return SessionHelper.userData().role === role ? true : false;
     }
 
 }
